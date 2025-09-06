316d6ff02c18ba6a1c3a05c078900f216984a51e
Mihai Voina <mihaitza1058@yahoo.com>
2025-09-06 14:41:37 +0300

    add tailwind config and basic layout

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@/assets/styles/globals.css';
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div className="text-red-600">Home Page</div>;
+};
+
+export default HomePage;
