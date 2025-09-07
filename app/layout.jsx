42875dbafd2f008c4124863ea34e6d701c94b3bb
Mihai Voina <mihaitza1058@yahoo.com>
2025-09-07 18:11:57 +0300

    feat: integrate NextAuth for authentication with Google provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
   title: {
@@ -15,13 +16,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
