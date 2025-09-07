04f34d587dace176e348fbe6171f736607ff2565
Mihai Voina <mihaitza1058@yahoo.com>
2025-09-07 15:17:08 +0300

    add home page property listings,not found page, loading page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero';
+import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
 
 export const metadata = {
@@ -9,6 +10,7 @@ export default function HomePage() {
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
