4236ae4400a974c4b8b72e37a3ec72d4e938fd3a
Mihai Voina <mihaitza1058@yahoo.com>
2025-09-12 14:29:49 +0300

    feat: enhance PropertiesPage with search and pagination; add PropertySearchForm and Pagination components; integrate lightbox for property images

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,6 +1,17 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
 const PropertyImages = ({ images }) => {
+  const [index, setIndex] = useState(-1);
+
   return (
     <section className="bg-blue-50 p-4">
       <div className="container mx-auto">
@@ -8,31 +19,41 @@ const PropertyImages = ({ images }) => {
           <Image
             src={images[0]}
             alt=""
-            className="object-cover h-[400px] mx-auto rounded-xl"
+            className="object-cover h-[400px] mx-auto rounded-xl cursor-pointer"
             width={1800}
             height={400}
-            priority={true}
+            priority
+            onClick={() => setIndex(0)}
           />
         ) : (
           <div className="grid grid-cols-2 gap-4">
-            {images.map((image, index) => (
+            {images.map((image, i) => (
               <div
-                key={index}
-                className={`${images.length === 3 && index === 2 ? 'col-span-2' : 'col-span-1'}`}
+                key={i}
+                className={`${images.length === 3 && i === 2 ? 'col-span-2' : 'col-span-1'}`}
               >
                 <Image
                   src={image}
                   alt=""
-                  className="object-cover h-[400px] w-full rounded-xl"
+                  className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
                   width={1800}
                   height={400}
-                  priority={true}
+                  priority
+                  onClick={() => setIndex(i)}
                 />
               </div>
             ))}
           </div>
         )}
       </div>
+
+      <Lightbox
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        slides={images.map((src) => ({ src }))}
+        plugins={[Fullscreen, Zoom, Thumbnails]}
+      />
     </section>
   );
 };
